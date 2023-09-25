6c54c0113c87ac0d043dcdfb8f3dea9340a19d3a
RBFERNANDESS <145044363+RBFERNANDESS@users.noreply.github.com>
2023-09-25 17:35:03 -0300

    atividade

--- supermecado.py
+++ supermecado.py
@@ -1,10 +1,22 @@
-produto= ["arroz", "feijao", "açucar", "café", "leite"]
-estoque =[10, 3, 1, 20, 5]
-preco=[10.00, 5.00, 7.00, 3.00, 8.00]
 
-for b in range (len(produto)):
-    produtos = produto [b]
-    estoques = estoque [b]
-    precos = preco [b]
-    totals = estoque [b]*preco[b]
-    print(f"produto: {produtos}, estoque: {estoques}, preços {precos}, Total {totals}")
+prod=["arroz","feijao","açucar","cafe","sal"]
+custos=[15,5,3,4,2]
+estoque=[10,8,7,6,5]
+imp1=0.12
+imp2=0.06
+imp3=0.03
+frete=50
+for b in range(len(prod)):
+    produto=prod[b]
+    custo=custos[b]
+    estoq=estoque[b]
+    lucro=0.60
+    custoimp1=(custo*imp1)
+    custoimp2=(custo*imp2)
+    custoimp3=(custo*imp3)
+    frete=((50/estoq))
+    precocusto=(custo+custoimp1+custoimp2+custoimp3)
+    margemdelucro=(precocusto*lucro)
+    precovenda=(precocusto+margemdelucro)
+    precovendacomfrete=(precovenda+frete)
+    print(f"produto: , {produto}, custo s/ imposto:, {custo:.2f}, custo c/ imposto: , {precocusto:.2f}, preço de venda: , {precovenda:.2f}, preço de venda com frete: , {precovendacomfrete:.2f}" )
